d61010afdc2ec3304ed8118b9b0cab2a2e0bb102
Willian Ferreira <willianhf15@gmail.com>
2019-06-07 14:10:54 -0300

    Transform Container style into component

---
+++ mobile/twillian/src/components/Container/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  background-color: #f5f8fa;
+  border-radius: 5px;
+
+  margin: 5px 15px;
+  padding: 10px 15px;
+`;
+
+export default Container;
