c425f6916b17a64a96d413dff95dab698df993e3
Willian Ferreira <willianhf15@gmail.com>
2019-06-09 16:02:45 -0300

    Create the home page

--- mobile/twillian/src/pages/Home/index.js
+++ mobile/twillian/src/pages/Home/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container, Card, Input, Login, LoginText, Register, RegisterText,
+} from './styles';
 
 export default function Home() {
-  return <Container />;
+  return (
+    <Container>
+      <Card>
+        <Input placeholder="Usuário" />
+        <Input
+          placeholder="Senha"
+          secureTextEntry
+          autoCompleteType="password"
+          textContentType="password"
+        />
+        <Login>
+          <LoginText>Entrar</LoginText>
+        </Login>
+        <Register>
+          <RegisterText>Registrar-se</RegisterText>
+        </Register>
+      </Card>
+    </Container>
+  );
 }
--- mobile/twillian/src/pages/Home/styles.js
+++ mobile/twillian/src/pages/Home/styles.js
@@ -1,8 +1,58 @@
 import styled from 'styled-components/native';
 
-import RawContainer from '~/components/Container';
-
-export const Container = styled(RawContainer)`
+export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+
   background-color: #1da1f2;
+`;
+
+export const Card = styled.View`
+  background-color: #ffffff;
+
+  border-radius: 5px;
+  margin: 25px;
+  padding: 40px;
+`;
+
+export const Input = styled.TextInput`
+  border-color: #1da1f2;
+  border-width: 1px;
+  border-radius: 5px;
+
+  color: #14171a;
+
+  margin-bottom: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+`;
+
+export const Login = styled.TouchableOpacity`
+  background-color: #1da1f2;
+
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 15px;
+`;
+
+export const LoginText = styled.Text`
+  text-align: center;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+export const Register = styled.TouchableOpacity`
+  border-radius: 5px;
+  border-width: 1px;
+  border-color: #1da1f2;
+  background-color: transparent;
+
+  margin-top: 15px;
+  padding: 10px;
+`;
+
+export const RegisterText = styled.Text`
+  text-align: center;
+  font-weight: bold;
+  color: #1da1f2;
 `;
