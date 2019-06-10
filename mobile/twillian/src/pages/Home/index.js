3d48db7c9de944979cb767c0849aaaf35306da3f
Willian Ferreira <willianhf15@gmail.com>
2019-06-10 14:23:19 -0300

    Go to the next input instead close the keyboard

--- mobile/twillian/src/pages/Home/index.js
+++ mobile/twillian/src/pages/Home/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Container, Card, Input, Login, LoginText, Register, RegisterText,
 } from './styles';
 
 export default function Home({ navigation }) {
+  const refs = {};
+
   return (
     <Container>
       <Card>
-        <Input placeholder="Usuário" />
         <Input
+          placeholder="Usuário"
+          returnKeyType="next"
+          onSubmitEditing={() => refs.password.focus()}
+          blurOnSubmit={false}
+        />
+        <Input
+          ref={input => (refs.password = input)}
           placeholder="Senha"
           secureTextEntry
           autoCompleteType="password"
