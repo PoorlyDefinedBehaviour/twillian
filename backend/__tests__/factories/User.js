e0c5c8dabb838e1c946ffca55908015b6ce49cf9
Bruno <brunotj2015@hotmail.com>
2019-06-12 14:41:02 -0300

    separates backend into a new branch

--- backend/__tests__/factories/User.js
+++
@@ -1,7 +0,0 @@
-const RandomString = require("randomstring");
-
-module.exports = () => ({
-  username: RandomString.generate(15),
-  email: `${RandomString.generate(5)}@${RandomString.generate(5)}.com`,
-  password: RandomString.generate(15)
-});
