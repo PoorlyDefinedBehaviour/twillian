f28451efd0e0c6ee45136b92464fca6e09c64beb
Willian Ferreira <willianhf15@gmail.com>
2019-06-13 20:13:54 -0300

    Add form and validations to register page

--- mobile/twillian/src/pages/Register/index.js
+++ mobile/twillian/src/pages/Register/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import AsyncStorage from 'react-native';
+
+import { withNavigation } from 'react-navigation';
+
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import api from '~/services/api';
 
 import {
-  Card, Container, Logo, LogoContainer, Input, Login, LoginText,
+  Card, Container, Logo, LogoContainer, Input, Error, Login, LoginText,
 } from './styles';
 
 import logo from '~/assets/images/logo.png';
@@ -9,51 +17,128 @@ import logo from '~/assets/images/logo.png';
 function Register({ navigation }) {
   const refs = {};
 
+  const initialValues = {
+    username: '',
+    email: '',
+    password: '',
+    confirm: '',
+  };
+
+  const schema = Yup.object().shape({
+    username: Yup.string()
+      .required('Digite um nome de usuário.')
+      .min(5, 'O nome de usuário deve ter no mínimo 5 caracteres.')
+      .max(30, 'O nome de usuário deve ter no máximo 5 caracteres.'),
+    email: Yup.string()
+      .email('Digite um e-mail válido.')
+      .required('Digite um e-mail válido.'),
+    password: Yup.string()
+      .required('Sua senha não pode ficar vazia.')
+      .min(5, 'Sua senha deve ter no mínimo 5 caracteres.')
+      .max(30, 'Sua senha deve ter no máximo 5 caracteres.'),
+    confirm: Yup.string()
+      .required('Confirme sua senha.')
+      .oneOf([Yup.ref('password'), null], 'As duas senhas devem ser iguais.'),
+  });
+
+  const onSubmit = async (data, { setSubmitting, setErrors }) => {
+    try {
+      setSubmitting(true);
+
+      const response = await api.post('signup', data);
+
+      navigation.navigate('Timeline');
+
+      const { user, token } = response.data;
+      await AsyncStorage.setItem('@twillian:token', token);
+      await AsyncStorage.setItem('@twillian:user', user);
+    } catch (ex) {
+      // TODO: Handle exceptions
+      setErrors(ex.response.data);
+    }
+  };
+
   return (
-    <Container>
-      <LogoContainer>
-        <Logo source={logo} />
-      </LogoContainer>
-      <Card>
-        <Input
-          placeholder="Usuário"
-          returnKeyType="next"
-          onSubmitEditing={() => refs.email.focus()}
-          blurOnSubmit={false}
-        />
-        <Input
-          ref={input => (refs.email = input)}
-          placeholder="E-mail"
-          returnKeyType="next"
-          onSubmitEditing={() => refs.password.focus()}
-          blurOnSubmit={false}
-        />
-        <Input
-          ref={input => (refs.password = input)}
-          placeholder="Senha"
-          secureTextEntry
-          autoCompleteType="password"
-          textContentType="password"
-          onSubmitEditing={() => refs.confirm.focus()}
-          blurOnSubmit={false}
-        />
-        <Input
-          ref={input => (refs.confirm = input)}
-          placeholder="Senha"
-          secureTextEntry
-          autoCompleteType="password"
-          textContentType="password"
-        />
-        <Login onPress={() => navigation.navigate('Timeline')}>
-          <LoginText>Registrar</LoginText>
-        </Login>
-      </Card>
-    </Container>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={schema}
+      validateOnBlur
+      validateOnChange
+    >
+      {({ values, handleChange, handleBlur, touched, errors, handleSubmit }) => (
+        <Container>
+          <LogoContainer>
+            <Logo source={logo} />
+          </LogoContainer>
+          <Card>
+            <Input
+              placeholder="Usuário"
+              returnKeyType="next"
+              onSubmitEditing={() => refs.email.focus()}
+              value={values.username}
+              onChangeText={handleChange('username')}
+              onBlur={handleBlur('username')}
+              blurOnSubmit={false}
+              error={touched.username && !!errors.username}
+            />
+            {touched.username && !!errors.username && <Error>{errors.username}</Error>}
+            <Input
+              ref={input => (refs.email = input)}
+              placeholder="E-mail"
+              returnKeyType="next"
+              autoCompleteType="email"
+              textContentType="emailAddress"
+              keyboardType="email-address"
+              onSubmitEditing={() => refs.password.focus()}
+              value={values.email}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              blurOnSubmit={false}
+              error={touched.email && !!errors.email}
+            />
+            {touched.email && !!errors.email && <Error>{errors.email}</Error>}
+            <Input
+              ref={input => (refs.password = input)}
+              placeholder="Senha"
+              secureTextEntry
+              autoCompleteType="password"
+              textContentType="password"
+              onSubmitEditing={() => refs.confirm.focus()}
+              value={values.password}
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              blurOnSubmit={false}
+              error={touched.password && !!errors.password}
+            />
+            {touched.password && !!errors.password && <Error>{errors.password}</Error>}
+            <Input
+              ref={input => (refs.confirm = input)}
+              placeholder="Confirmar senha"
+              secureTextEntry
+              autoCompleteType="password"
+              textContentType="password"
+              value={values.confirm}
+              onChangeText={handleChange('confirm')}
+              onBlur={handleBlur('confirm')}
+              error={touched.confirm && !!errors.confirm}
+            />
+            {touched.confirm && !!errors.confirm && <Error>{errors.confirm}</Error>}
+            <Login
+              disabled={Object.entries(errors).length > 0 || Object.entries(touched).length == 0}
+              onPress={handleSubmit}
+            >
+              <LoginText>Registrar</LoginText>
+            </Login>
+          </Card>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
 export default {
-  screen: Register,
+  screen: withNavigation(Register),
   navigationOptions: {
     headerStyle: {
       elevation: 0,
