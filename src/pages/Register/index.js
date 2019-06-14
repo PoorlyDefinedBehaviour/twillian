ba0a381afc9c9185415c4b0288ddec387e082642
Bruno <brunotj2015@hotmail.com>
2019-06-14 16:05:49 -0300

    finishes register page layout

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+import DefaultUser from "../../assets/defaultuser.jpg";
+import AppStore from "../../assets/applestore.png";
+import GooglePlay from "../../assets/googleplay.png";
+
 import {
   Container,
   UsernameInput,
@@ -7,22 +11,35 @@ import {
   PasswordInput,
   RegisterButton,
   RegisterForm,
-  PageBox
+  PageBox,
+  DefaultUserImage,
+  DownloadContainer,
+  AppStoreImage,
+  GooglePlayImage
 } from "./styles";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault();
     console.log(username, email, password);
+
+    const user = {
+      username,
+      email,
+      password,
+      passwordConfirmation
+    };
   };
 
   return (
     <PageBox>
       <Container>
+        <DefaultUserImage src={DefaultUser} />
         <RegisterForm onSubmit={handleSubmit}>
           <UsernameInput
             placeholder="Nome de usuário"
@@ -39,9 +56,19 @@ export default function Register() {
             value={password}
             onChange={event => setPassword(event.target.value)}
           />
+          <PasswordInput
+            placeholder="Confirme sua senha"
+            value={passwordConfirmation}
+            onChange={event => setPasswordConfirmation(event.target.value)}
+          />
           <RegisterButton>Registrar</RegisterButton>
         </RegisterForm>
       </Container>
+
+      <DownloadContainer>
+        <AppStoreImage src={AppStore} />
+        <GooglePlayImage src={GooglePlay} />
+      </DownloadContainer>
     </PageBox>
   );
 }
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,60 +1,88 @@
 import styled from "styled-components";
 
+export const DefaultUserImage = styled.img`
+  width: 40%;
+  border-radius: 50%;
+  border: 3px solid #fff;
+  margin-top: -20%;
+`;
+
 export const PageBox = styled.div`
   width: 100%;
   height: 100vh;
   background-color: #e6ecf0;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
 export const Container = styled.div`
   background-color: #fff;
-  width: 300px;
-  height: 400px;
+  width: 325px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  box-shadow: 0px 3px 62px -52px rgba(0, 0, 0, 0.75);
+  border-radius: 5px;
 `;
 
 export const RegisterForm = styled.form`
   width: 80%;
   text-align: center;
-  margin-top: 25%;
 `;
 
 export const UsernameInput = styled.input`
   font-family: "montserrat", sans-serif;
-  background-color: #f5f8f1;
+  background-color: #f4f8fa;
   padding: 15px;
-  margin: 10px;
-  border-radius: 2px;
+  margin: 15px;
+  border-radius: 5px;
 `;
 
 export const EmailInput = styled.input`
   font-family: "montserrat", sans-serif;
-  background-color: #f5f8f1;
+  background-color: #f4f8fa;
   padding: 15px;
-  margin: 10px;
-  border-radius: 2px;
+  margin: 15px;
+  border-radius: 5px;
 `;
 
 export const PasswordInput = styled.input`
   font-family: "montserrat", sans-serif;
-  background-color: #f5f8f1;
+  background-color: #f4f8fa;
   padding: 15px;
-  margin: 10px;
-  border-radius: 2px;
+  margin: 15px;
+  border-radius: 5px;
 `;
 
 export const RegisterButton = styled.button`
   font-family: "montserrat", sans-serif;
   font-weight: bold;
-  width: 80%;
+  width: 70%;
   color: #f1f1f1;
   background-color: #1da1f2;
   padding: 15px;
-  margin-top 20px;
-  border-radius: 2px;
+  margin-top: 20px;
+  margin-bottom: 40px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const DownloadContainer = styled.div`
+  background-color: #fff;
+  width: 325px;
+  display: flex;
+`;
+
+export const AppStoreImage = styled.img`
+  transform: scale(0.5);
+  cursor: pointer;
+  margin-left: -15%;
+`;
+
+export const GooglePlayImage = styled.img`
+  transform: scale(0.5);
+  cursor: pointer;
+  margin-left: -25%;
 `;
