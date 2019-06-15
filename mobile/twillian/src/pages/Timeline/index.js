9dca09dcef7b7237e00ffbb1d0ff58836b8594b0
Willian Ferreira <willianhf15@gmail.com>
2019-06-15 14:07:18 -0300

    Add interceptor to always send the jwt token

--- mobile/twillian/src/pages/Timeline/index.js
+++ mobile/twillian/src/pages/Timeline/index.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 import api from '~/services/api';
+import { getUser } from '~/services/auth';
 
 import { Container, Tweets } from './styles';
 import NewTweet from '~/components/NewTweet';
 import Tweet from '~/components/Tweet';
 
-function Timeline({ navigation }) {
+function Timeline() {
   const [tweets, setTweets] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function fetchTweets() {
+      const user = await getUser();
+
+      try {
+        const response = await api.get(`tweet/${user._id}/following/0`);
+        setTweets(response.data);
+      } catch (ex) {
+        console.log(ex.response);
+      }
+    }
+
+    fetchTweets();
+  }, []);
 
   function renderTweet({ item }) {
     return (
       <Tweet
-        author={item.author}
+        author={item.user}
         content={item.content}
-        likes={item.likes}
-        retweets={item.retweets}
-        comments={item.comments}
+        likes={item.likes.length}
+        retweets={item.retweets.length}
+        comments={item.comments.length}
       />
     );
   }
@@ -26,7 +40,7 @@ function Timeline({ navigation }) {
   return (
     <Container>
       <NewTweet />
-      <Tweets data={tweets} keyExtractor={item => `tweet-${item.id}`} renderItem={renderTweet} />
+      <Tweets data={tweets} keyExtractor={item => item._id} renderItem={renderTweet} />
     </Container>
   );
 }
---
+++ mobile/twillian/src/services/auth.js
@@ -0,0 +1,16 @@
+import { AsyncStorage } from 'react-native';
+
+export const APP_KEY = '@twillian:';
+
+export async function getUser() {
+  return JSON.parse(await AsyncStorage.getItem(`${APP_KEY}user`));
+}
+
+export async function isAuthenticated() {
+  return !!(await getUser());
+}
+
+export async function getToken() {
+  const user = await getUser();
+  return user.token;
+}
