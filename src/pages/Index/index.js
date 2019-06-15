d767ca274589d37c9882fb5ae9e950d4f7ea9673
c0llins <collins.dev@icloud.com>
2019-06-15 01:13:26 -0300

    Update Index Page

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,20 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
+  PageBox,
+  AppImagem,
   Container,
-  SigUpForm,
+  ContainerSignIn,
+  Logo,
+  SubTitle,
+  SignInForm,
   UsernameInput,
   PasswordInput,
-  SigUpButton
+  SignInButton,
+  LineWrap,
+  LineCenter,
+  Line,
+  SignUpButton,
+  SignIn,
+  DownloadContainer,
+  AppStoreImage,
+  GooglePlayImage
 } from "./styles";
+import PhoneImagem from "../../assets/img/app.png";
+import LogoImagem from "../../assets/img/logo.png";
+import AppStore from "../../assets/img/applestore.png";
+import GooglePlay from "../../assets/img/googleplay.png";
 
 export default function Index() {
   return (
-    <Container>
-      <SigUpForm>
-        <UsernameInput placeholder="Nome de usuário" />
-        <PasswordInput placeholder="Senha" />
-        <SigUpButton>Entrar</SigUpButton>
-      </SigUpForm>
-    </Container>
+    <PageBox>
+      <Container>
+        <AppImagem src={PhoneImagem} />
+        <ContainerSignIn>
+          <SignIn>
+            <Logo src={LogoImagem} />
+            <SubTitle>
+              Entre e compartilhe seus pensamentos com seus amigos!
+            </SubTitle>
+            <SignInForm>
+              <UsernameInput placeholder="Nome de usuário" />
+              <PasswordInput placeholder="Senha" />
+              <SignInButton>Entrar</SignInButton>
+              <LineWrap>
+                <Line />
+                <LineCenter>ou</LineCenter>
+                <Line />
+              </LineWrap>
+
+              <Link to="/register">
+                <SignUpButton>Registrar</SignUpButton>
+              </Link>
+            </SignInForm>
+          </SignIn>
+          <DownloadContainer>
+            <AppStoreImage src={AppStore} />
+            <GooglePlayImage src={GooglePlay} />
+          </DownloadContainer>
+        </ContainerSignIn>
+      </Container>
+    </PageBox>
   );
 }
