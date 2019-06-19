274ff93b07eee0486014e9f991f01bdfc81b6013
c0llins <collins.dev@icloud.com>
2019-06-18 21:52:39 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Register from "./pages/Register";
 import Index from "./pages/Index";
+import Timeline from "./pages/Timeline";
 
 export default function Routes() {
   return (
@@ -9,7 +10,7 @@ export default function Routes() {
       <Switch>
         <Route exact path="/" component={() => <Index />} />
         <Route path="/register" component={() => <Register />} />
-        <Route path="/timeline" component={() => <h1>timeline</h1>} />
+        <Route path="/timeline" component={() => <Timeline />} />
       </Switch>
     </BrowserRouter>
   );
