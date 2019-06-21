fe2c2d1517cd1f835ae9fff921a6df06ba91b0cf
Bruno <brunotj2015@hotmail.com>
2019-06-21 17:40:31 -0300

    modifies TweetSchema to create relationships

--- src/models/Tweet.js
+++ src/models/Tweet.js
@@ -1,51 +1,54 @@
 const Mongoose = require("../database/mongodb/MongoDB");
 const mongoosePaginate = require("mongoose-paginate");
 
-const TweetSchema = new Mongoose.Schema({
-  user: {
-    type: Mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  media: String,
-  retweets: [
-    {
+const TweetSchema = new Mongoose.Schema(
+  {
+    user: {
       type: Mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: false
-    }
-  ],
-  likes: [
-    {
-      type: Mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: false
-    }
-  ],
-  comments: [
-    {
-      content: {
-        type: String,
-        required: true
-      },
-      user: {
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    media: String,
+    retweets: [
+      {
+        type: Mongoose.Schema.Types.ObjectId,
+        ref: "Retweet",
+        required: false
+      }
+    ],
+    likes: [
+      {
         type: Mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now
+        required: false
       }
-    }
-  ],
-},{
-  timestamps: true
-})
+    ],
+    comments: [
+      {
+        content: {
+          type: String,
+          required: true
+        },
+        user: {
+          type: Mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now
+        }
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
 
 TweetSchema.plugin(mongoosePaginate);
 
