9b458cbacc558675197fe5152bfb46c8ae6f5f71
Willian Ferreira <willianhf15@gmail.com>
2019-06-26 08:40:03 -0300

    Apply theme provider and global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
-import Routes from "./routes";
+import React from 'react';
+
+import { ThemeProvider } from 'styled-components';
+
+import Routes from './routes';
+
+import theme from './themes';
+
+import GlobalStyle from './styles';
 
 function App() {
-  return <Routes />;
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <Routes />
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    border: none;
+  }
+
+  body {
+    @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
+
+    font-family: "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
+      "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export default GlobalStyle;
