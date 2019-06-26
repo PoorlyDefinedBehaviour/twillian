5cae2a1b7cd6f5b67fef149786c4be276316d069
Willian Ferreira <willianhf15@gmail.com>
2019-06-26 11:52:44 -0300

    Add colors and shadow to theme

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,5 +1,12 @@
 export default {
   colors: {
-    primary: '#1da1f2'
-  }
+    primary: '#1da1f2',
+    black: '#14171a',
+    dark_gray: '#657786',
+    light_gray: '#aab8c2',
+    extra_light_gray: '#e1e8ed',
+    extra_extra_light_gray: '#f4f8fa',
+    error: '#e74c3c'
+  },
+  shadow: '0px 0px 50px 0px rgba(0,0,0,0.3)'
 };
